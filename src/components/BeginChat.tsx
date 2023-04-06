9f8ed82129fb5248ae6d19dbc497218c225a2398
Clay Smith <smithclay@gmail.com>
2023-04-05 19:40:33 -0700

    add workaround for TTS on iOS devices

--- src/components/BeginChat.tsx
+++ src/components/BeginChat.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Typography, Button, Grid } from '@mui/material';
+import React from "react";
+import { Typography, Button, Grid } from "@mui/material";
 import { ChatBubbleOutline } from "@mui/icons-material";
 
 interface MyComponentProps {
@@ -7,21 +7,44 @@ interface MyComponentProps {
   onClick: () => void;
 }
 
+// This is a workaround to enable TTS on iOS devices.
+const enableAutoTTS = () => {
+  if (typeof window === "undefined") {
+    return;
+  }
+  const isiOS =
+    navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+  if (!isiOS) {
+    return;
+  }
+  const simulateSpeech = () => {
+    const lecture = new SpeechSynthesisUtterance("hello");
+    lecture.volume = 0;
+    speechSynthesis.speak(lecture);
+    document.removeEventListener("click", simulateSpeech);
+  };
+
+  document.addEventListener("click", simulateSpeech);
+};
+
 const BeginChat: React.FC<MyComponentProps> = ({ loading, onClick }) => {
   return (
     <>
-        <Typography variant="h6">
-            Click the button to start a chat with a LLM bot.
-        </Typography>
-        <Button
-            variant="contained"
-            startIcon={<ChatBubbleOutline />}
-            disabled={loading}
-            onClick={onClick}
-            sx={{ marginTop: "12px" }}
-        >
-            {loading ? "Loading..." : "Start Chat"}
-        </Button>
+      <Typography variant="h6">
+        Click the button to start a chat with a LLM bot.
+      </Typography>
+      <Button
+        variant="contained"
+        startIcon={<ChatBubbleOutline />}
+        disabled={loading}
+        onClick={() => {
+          enableAutoTTS();
+          onClick();
+        }}
+        sx={{ marginTop: "12px" }}
+      >
+        {loading ? "Loading..." : "Start Chat"}
+      </Button>
     </>
   );
 };
